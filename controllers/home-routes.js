1610519e90c278b98bb3b226e342e80df58a562a
Jacastro619 <jorgecastro619@gmail.com>
2023-11-08 10:35:29 -0800

    Added endpoints to see a singular post and posts assigned to the user

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { User, Post } = require("../models");
 const { withAuth, areAuth } = require("../utils/auth");
 const sequelize = require("../config/connection");
-const { Post, User } = require("../models");
 
 // route to home page
 router.get("/", async (req, res) => {
@@ -17,8 +16,42 @@ router.get("/jobs", async (req, res) => {
     });
 
     const posts = dbPostData.map((post) => post.get({ plain: true }));
-// shows all the jobs if the user is logged in
-    res.render("jobs", {posts, loggedIn: req.session.loggedIn});
+    // shows all the jobs if the user is logged in
+    // res.render("jobs", { posts, loggedIn: req.session.loggedIn });
+    res.json(dbPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/jobs/:id", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ["username", "email"] }],
+    });
+
+    const post = dbPostData.get({ plain: true });
+
+    // res.render("one-job", { post, loggedIn: req.session.loggedIn });
+    res.json(dbPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/jobs/user/posts", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [{ model: User, attributes: ["username", "email"] }],
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    // res.render("user-jobs", { posts, loggedIn: req.session.loggedIn });
+    res.json(dbPostData);
   } catch (err) {
     res.status(500).json(err);
   }
