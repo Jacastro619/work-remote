63da021a2e578a16d47c532b750e84631812e4c7
stevensillsii <stevensills2@gmail.com>
2023-11-08 13:30:44 -0800

    seed data for postData.js updated with created on dates

--- seeds/postData.js
+++ seeds/postData.js
@@ -15,6 +15,7 @@ const postData = [
     job_title: "Need a Writer!",
     job_description: "I need a skilled Writer that can write great content",
     job_budget: 150,
+    timestamp: "Created on Dec 2nd 2023",
     user_id: 4,
   },
   {
@@ -23,6 +24,7 @@ const postData = [
     job_description:
       "I need a skilled web designer that can design a website for my restaurant",
     job_budget: 250,
+    timestamp: "Created on Nov 18th 2023",
     user_id: 2,
   },
   {
@@ -31,6 +33,7 @@ const postData = [
     job_description:
       "Looking for a talented graphic designer to create a unique logo for my startup",
     job_budget: 100,
+    timestamp: "Created on Jan 9th 2022",
     user_id: 3,
   },
   {
@@ -39,6 +42,7 @@ const postData = [
     job_description:
       "Looking for a content writer to create engaging blog posts",
     job_budget: 100,
+    timestamp: "Created on Feb 8th 2021",
     user_id: 4,
   },
   {
@@ -46,6 +50,7 @@ const postData = [
     job_title: "Design a company logo",
     job_description: "Need a talented graphic designer to create a unique logo",
     job_budget: 150,
+    timestamp: "Created on May 12th 2023",
     user_id: 1,
   },
   {
@@ -54,6 +59,7 @@ const postData = [
     job_description:
       "Seeking a web developer to create a responsive online store",
     job_budget: 300,
+    timestamp: "Created on Jun 1st 2019",
     user_id: 3,
   },
   {
@@ -62,6 +68,7 @@ const postData = [
     job_description:
       "Digital marketing expert needed for a social media advertising campaign",
     job_budget: 200,
+    timestamp: "Created on Oct 22nd 2023",
     user_id: 14,
   },
   {
@@ -70,6 +77,7 @@ const postData = [
     job_description:
       "Hire a social media manager to handle your online presence",
     job_budget: 180,
+    timestamp: "Created on Mar 4th 2018",
     user_id: 15,
   },
   {
@@ -78,6 +86,7 @@ const postData = [
     job_description:
       "Copywriter needed to write compelling product descriptions",
     job_budget: 120,
+    timestamp: "Created on Nov 8th 2022",
     user_id: 16,
   },
   {
@@ -85,6 +94,7 @@ const postData = [
     job_title: "Administrative support",
     job_description: "Virtual assistant to help with administrative tasks",
     job_budget: 140,
+    timestamp: "Created on Mar 29th 2021",
     user_id: 17,
   },
   {
@@ -93,6 +103,7 @@ const postData = [
     job_description:
       "Assistance in setting up an e-commerce store for dropshipping",
     job_budget: 250,
+    timestamp: "Created on Nov 8th 2004",
     user_id: 18,
   },
   {
@@ -100,6 +111,7 @@ const postData = [
     job_title: "Online math tutor",
     job_description: "Math tutor for online teaching sessions",
     job_budget: 90,
+    timestamp: "Created on Jan 4th 2022",
     user_id: 19,
   },
   {
@@ -107,6 +119,7 @@ const postData = [
     job_title: "Data entry and analysis",
     job_description: "Data entry and analysis work for a research project",
     job_budget: 130,
+    timestamp: "Created on Mar 17th 2021",
     user_id: 20,
   },
   // Additional entries to reach a total of 40
@@ -115,6 +128,7 @@ const postData = [
     job_title: "Write product reviews",
     job_description: "Content writer to write detailed product reviews",
     job_budget: 80,
+    timestamp: "Created on Sep 21st 2023",
     user_id: 16,
   },
   {
@@ -123,6 +137,7 @@ const postData = [
     job_description:
       "Graphic designer to create an eye-catching promotional poster",
     job_budget: 90,
+    timestamp: "Created on Oct 26th 2023",
     user_id: 14,
   },
   {
@@ -130,6 +145,7 @@ const postData = [
     job_title: "Redesign an existing website",
     job_description: "Web developer needed to revamp an existing website",
     job_budget: 220,
+    timestamp: "Created on Feb 14th 2023",
     user_id: 3,
   },
   {
@@ -137,6 +153,7 @@ const postData = [
     job_title: "Run Google Ads campaign",
     job_description: "Digital marketing specialist for a Google Ads campaign",
     job_budget: 160,
+    timestamp: "Created on Apr 23th 2023",
     user_id: 18,
   },
   {
@@ -144,6 +161,7 @@ const postData = [
     job_title: "Create Instagram content",
     job_description: "Social media marketer to design engaging Instagram posts",
     job_budget: 110,
+    timestamp: "Created on May 23th 2023",
     user_id: 11,
   },
   {
@@ -151,6 +169,7 @@ const postData = [
     job_title: "Write email newsletters",
     job_description: "Copywriter to craft persuasive email newsletters",
     job_budget: 70,
+    timestamp: "Created on Jan 12th 2022",
     user_id: 6,
   },
   {
@@ -158,6 +177,7 @@ const postData = [
     job_title: "Manage calendar and appointments",
     job_description: "Virtual assistant to organize schedules and appointments",
     job_budget: 130,
+    timestamp: "Created on Jul 18th 2023",
     user_id: 7,
   },
   {
@@ -166,6 +186,7 @@ const postData = [
     job_description:
       "Assist in finding profitable products for an e-commerce business",
     job_budget: 180,
+    timestamp: "Created on Aug 1st 2023",
     user_id: 8,
   },
   {
@@ -173,6 +194,7 @@ const postData = [
     job_title: "Teach Spanish online",
     job_description: "Online Spanish tutor for language lessons",
     job_budget: 120,
+    timestamp: "Created on Jan 10th 2021",
     user_id: 1,
   },
   {
@@ -180,6 +202,7 @@ const postData = [
     job_title: "Data cleaning and analysis",
     job_description: "Data entry and cleaning for a research project",
     job_budget: 110,
+    timestamp: "Created on Dec 11th 2022",
     user_id: 1,
   },
   // Continue adding more entries as needed
