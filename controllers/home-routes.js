b3c06180826049236c33b17b81fafbae37ab9d63
Jacastro619 <jorgecastro619@gmail.com>
2023-11-08 14:04:57 -0800

    added hrefs to anchor tags in handlebars files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -18,12 +18,12 @@ router.get("/jobs", async (req, res) => {
     let posts = dbPostData.map((post) => post.get({ plain: true }));
     if (!req.session.loggedIn) {
       posts = posts.slice(0, 6);
-      res.json(posts);
+      res.render("jobs", { posts, loggedIn: req.session.loggedIn });
       return;
     }
     // shows all the jobs if the user is logged in
-    // res.render("jobs", { posts, loggedIn: req.session.loggedIn });
-    res.json(posts);
+    res.render("jobs", { posts, loggedIn: req.session.loggedIn });
+    // res.json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -37,8 +37,8 @@ router.get("/jobs/:id", withAuth, async (req, res) => {
 
     const post = dbPostData.get({ plain: true });
 
-    // res.render("one-job", { post, loggedIn: req.session.loggedIn });
-    res.json(dbPostData);
+    res.render("one-job", { post, loggedIn: req.session.loggedIn });
+    // res.json(dbPostData);
   } catch (err) {
     res.status(500).json(err);
   }
